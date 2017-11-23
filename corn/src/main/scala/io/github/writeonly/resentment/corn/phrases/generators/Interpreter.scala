5a80e9bc937cb48c52120a1434a791ca18d2d234
Kamil Adam Żabiński <writeonly@gmail.com>
2017-11-23 22:19:58 +0100

    Interpreter

--- corn/src/main/scala/io/github/writeonly/resentment/corn/phrases/generators/Interpreter.scala
+++ corn/src/main/scala/io/github/writeonly/resentment/corn/phrases/generators/Interpreter.scala
@@ -7,7 +7,17 @@ import scala.collection.mutable
 class Interpreter extends Generator {
   val b = new mutable.HashMap[Symbol, Int]()
   var a = 0
-  val o = new StringBuilder
+  val out = new StringBuilder
+
+  def uvar(o:Symbol) = b.put(o, a)
+  def ust(o:Symbol) = b.put(o, a)
+
+  def uld(o:Symbol) =  a = b(o)
+  def uld(o:Char) = a = o.toInt
+  def uld(o:String) = a =  o.toInt
+  def uld(o:BigDecimal) = a = o.bigDecimal.toBigInteger.intValue()
+
+  def pout() = out.append(a.toChar)
 
   val partial = new PartialFunction[Command, Unit] {
     override def isDefinedAt(x: Command): Boolean = x != null
@@ -16,7 +26,7 @@ class Interpreter extends Generator {
 
   override def apply(code: Command): String = {
     eval(code)
-    o.toString()
+    out.toString()
   }
 
   def eval(terminal: Command):Unit = terminal match {
@@ -24,14 +34,14 @@ class Interpreter extends Generator {
       if (left!= null)  partial(left)
       if (right!= null) partial(right)
     }
-    case Var(x, symbol) => b.put(symbol, a)
-    case Store(x, symbol) => b.put(symbol, a)
-    case LoadVar(c) => a = b(c)
-    case LoadChar(c) => a = c.toInt
-    case UnaryOperation("OUT", x) => o.append(a.toChar)
-    case UnaryOperation("NOT", x) => o.append(a.toChar)
-    case UnaryOperation("NEG", x) => o.append(a.toChar)
-    case LoadDecinal(x) => a = x.bigDecimal.toBigInteger.intValue()
+    case Var(_, symbol) => uvar(symbol)
+    case Store(_, symbol) => ust(symbol)
+    case LoadVar(c) => uld(c)
+    case LoadChar(c) => uld(c)
+    case LoadDecinal(c) => uld(c)
+    case UnaryOperation("OUT", _) => pout()
+    case UnaryOperation("NOT", _) => pout()
+    case UnaryOperation("NEG", _) => pout()
 //    case BinaryOperation("ADD", x1, x2) => (apply(x1) + apply(x2))
 //    case BinaryOperation("SUB", x1, x2) => (apply(x1) - apply(x2))
 //    case BinaryOperation("MUL", x1, x2) => (apply(x1) * apply(x2))
--- corn/src/main/scala/io/github/writeonly/resentment/corn/phrases/generators/InterpreterStack.scala
+++ corn/src/main/scala/io/github/writeonly/resentment/corn/phrases/generators/InterpreterStack.scala
@@ -4,36 +4,53 @@ import io.github.writeonly.resentment.corn.command._
 
 import scala.collection.mutable
 
-class InterpreterStack extends Interpreter {
+class InterpreterStack extends Generator {
+  val b = new mutable.HashMap[Symbol, Int]()
+  var a = 0
+  val out = new StringBuilder
 
   val m = new mutable.HashMap[Int, Int]()
-
   var p = 0
 
   def get(symbol: Symbol) = b.get(symbol).get
 
-  def put(symbol:Symbol) = m.put(get(symbol), a)
-
   def uvar(o: Symbol) = {
     b.put(o, p)
     p += 1
-    put(o)
+    ust(o)
   }
 
-  override def eval(terminal: Command):Unit = terminal match {
+  def ust(symbol:Symbol) = m.put(get(symbol), a)
+
+  def uld(o:Symbol) = a =  m.get(get(o)).get
+  def uld(o:Char) = a = o.toInt
+  def uld(o:String) = a =  o.toInt
+  def uld(o:BigDecimal) = a = o.bigDecimal.toBigInteger.intValue()
+  def pout() = out.append(a.toChar)
+
+  val partial = new PartialFunction[Command, Unit] {
+    override def isDefinedAt(x: Command): Boolean = x != null
+    override def apply(v1: Command): Unit = eval(v1)
+  }
+
+  override def apply(code: Command): String = {
+    eval(code)
+    out.toString()
+  }
+
+  def eval(terminal: Command):Unit = terminal match {
     case PairInstruction(left, right) => {
       if (left!= null) partial(left)
       if (right!= null) partial(right)
     }
-    case Var(x, symbol) => uvar(symbol)
-    case Store(x, symbol) => put(symbol)
-    case LoadVar(symbol) => a =  m.get(get(symbol)).get
-    case LoadChar(c) => a = c.toInt
-    case LoadStr(c) => a = c.toInt
-    case UnaryOperation("OUT", x) => o.append(a.toChar)
-    case UnaryOperation("NOT", x) => o.append(a.toChar)
-    case UnaryOperation("NEG", x) => o.append(a.toChar)
-    case LoadDecinal(x) => a = x.bigDecimal.toBigInteger.intValue()
+    case Var(_, symbol) => uvar(symbol)
+    case Store(_, symbol) => ust(symbol)
+    case LoadVar(c) => uld(c)
+    case LoadChar(c) => uld(c)
+    case LoadDecinal(c) => uld(c)
+    case UnaryOperation("OUT", _) => pout()
+    case UnaryOperation("NOT", _) => pout()
+    case UnaryOperation("NEG", _) => pout()
 //    case BinaryOperation("ADD", x1, x2) => (apply(x1) + apply(x2))
 //    case BinaryOperation("SUB", x1, x2) => (apply(x1) - apply(x2))
 //    case BinaryOperation("MUL", x1, x2) => (apply(x1) * apply(x2))
