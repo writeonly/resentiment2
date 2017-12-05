28659ab6b12422c74a661dd56952b3b797eac9ba
Kamil Adam Żabiński <writeonly@gmail.com>
2017-12-05 22:08:07 +0100

    InterpreterBF

--- fsm/src/main/scala/io/github/writeonly/resentment/fsm/InterpreterBF.scala
+++ fsm/src/main/scala/io/github/writeonly/resentment/fsm/InterpreterBF.scala
@@ -4,46 +4,49 @@ import java.io.{Reader}
 
 import scala.collection.mutable
 
-class InterpreterBF {
+class InterpreterBF(code : Array[Byte]) {
   var counter = 0
   var head = 0
   val tape = new mutable.HashMap[Int,Int]()
+  val jumpTable = InterpreterBF.createJumpTable(code)
 
+  def apply(i:Byte) = i match {
+    case '+' => 
+    case '-' =>
+    case '>' =>
+    case '<' =>
+    case '[' =>
+    case ']' =>
+    case ',' =>
+    case '.' =>
+  }
 
-  def apply(code :String):Unit = apply(code.getBytes)
+}
+
+object InterpreterBF {
+  def createJumpTable(code:Array[Byte]): Map[Int,Int] = {
+    val result = new mutable.HashMap[Int,Int]()
+    val stack = new mutable.Stack[Int]()
+    for((x,i) <- code.view.zipWithIndex) {
+      x match  {
+        case '[' => stack.push(i)
+        case ']' =>  {
+          val j = stack.pop()
+          result(i) = j
+          result(j) = i
+        }
+        case _ =>
+      }
+    }
+    result.toMap
+  }
+
+  def apply(code :String):Unit = new InterpreterBF(code.getBytes)
 
   def apply(reader : Reader):Unit = {
 
     try Stream.continually(reader.read).takeWhile(-1 !=).map(_.toByte)
     finally reader.close()
-    
-  }
-
-  def apply(code : Array[Byte]): Unit = {
 
   }
-
-  def createJump(code:Array[Byte]): Map[Int,Int] = {
-    val result = new mutable.HashMap[Int,Int]()
-    val stack = new mutable.Stack[Int]()
-    for((x,i) <- code.view.zipWithIndex) {
-     x match  {
-       case '[' => stack.push(i)
-       case ']' =>  {
-         val j = stack.pop()
-         result(i) = j
-         result(j) = i
-       }
-       case _ =>
-     }
-    }
-    result.toMap
-  }
-
-
-
-  def apply(i:Int) = i match {
-    case '+' =>
-  }
-
 }
--- fsm/src/test/scala/io/github/writeonly/resentment/fsm/InterpreterBFSpec.scala
+++ fsm/src/test/scala/io/github/writeonly/resentment/fsm/InterpreterBFSpec.scala
@@ -6,18 +6,17 @@ import org.specs2.runner.JUnitRunner
 @RunWith(classOf[JUnitRunner])
 class InterpreterBFSpec extends org.specs2.mutable.Specification {
   "this is my specification" >> {
-    val interpreter = new InterpreterBF()
     "where example []" >> {
-      interpreter.createJump("[]".getBytes) must_== Map(0 -> 1, 1 -> 0)
+      InterpreterBF.createJumpTable("[]".getBytes) must_== Map(0 -> 1, 1 -> 0)
     }
     "where example [[]]" >> {
-      interpreter.createJump("[[]]".getBytes) must_== Map(2 -> 1, 1 -> 2, 3 -> 0, 0 -> 3)
+      InterpreterBF.createJumpTable("[[]]".getBytes) must_== Map(2 -> 1, 1 -> 2, 3 -> 0, 0 -> 3)
     }
     "where example [[]][]" >> {
-      interpreter.createJump("[[]][]".getBytes) must_== Map(0 -> 3, 5 -> 4, 1 -> 2, 2 -> 1, 3 -> 0, 4 -> 5)
+      InterpreterBF.createJumpTable("[[]][]".getBytes) must_== Map(0 -> 3, 5 -> 4, 1 -> 2, 2 -> 1, 3 -> 0, 4 -> 5)
     }
     "where example [][[]]" >> {
-      interpreter.createJump("[][[]]".getBytes) must_== Map(0 -> 1, 5 -> 2, 1 -> 0, 2 -> 5, 3 -> 4, 4 -> 3)
+      InterpreterBF.createJumpTable("[][[]]".getBytes) must_== Map(0 -> 1, 5 -> 2, 1 -> 0, 2 -> 5, 3 -> 4, 4 -> 3)
     }
   }
 }
