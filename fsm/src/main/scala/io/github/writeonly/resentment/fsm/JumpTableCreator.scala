3307694933da6622fb99c89f2b41037c4f983cd2
kamil-adam <kamil.zabinski@relayr.io>
2017-12-12 08:33:24 +0100

    Interpreter

--- fsm/src/main/scala/io/github/writeonly/resentment/fsm/JumpTableCreator.scala
+++ fsm/src/main/scala/io/github/writeonly/resentment/fsm/JumpTableCreator.scala
@@ -1,15 +1,20 @@
 package io.github.writeonly.resentment.fsm
 
 import scala.collection.mutable
+import scala.util.{Failure, Success, Try}
 
-class JumpTableCreator(code:Array[Byte], result :mutable.HashMap[Int,Int], stack :mutable.Stack[Int]) {
+case class JumpTableCreator(code:Array[Byte], result :mutable.HashMap[Int,Int], stack :mutable.Stack[Int]) {
   def this(code:Array[Byte]) = this(code, new mutable.HashMap[Int,Int](), new mutable.Stack[Int]())
 
   def this(code:String) = this(code.getBytes)
 
-
   def apply() :Map[Int, Int] = {
-    for((x,i) <- code.view.zipWithIndex) apply(i, x)
+    for((x,i) <- code.view.zipWithIndex) {
+      Try(apply(i, x)) match {
+        case Success(_) =>
+        case Failure(e) => throw new IllegalStateException(toString, e)
+      }
+    }
     result.toMap
   }
 
