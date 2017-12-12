e55449d4690fc358ec1870e57b92301bd3774461
kamil-adam <kamil.zabinski@relayr.io>
2017-12-12 07:06:25 +0100

    Interpreter

--- fsm/src/main/scala/io/github/writeonly/resentment/fsm/InterpreterBF.scala
+++ fsm/src/main/scala/io/github/writeonly/resentment/fsm/InterpreterBF.scala
@@ -44,18 +44,20 @@ object InterpreterBF {
   def createJumpTable(code:Array[Byte]): Map[Int,Int] = {
     val result = new mutable.HashMap[Int,Int]()
     val stack = new mutable.Stack[Int]()
-    for((x,i) <- code.view.zipWithIndex) {
-      x match  {
-        case '[' => stack.push(i)
-        case ']' =>  {
-          val j = stack.pop()
-          result(i) = j
-          result(j) = i
-        }
-        case _ =>
-      }
-    }
+    for((x,i) <- code.view.zipWithIndex) apply(result, stack, i, x)
     result.toMap
+  }
+
+  def apply(result :mutable.HashMap[Int,Int], stack :mutable.Stack[Int], i :Int, x :Byte) = x match {
+    case '[' => stack.push(i)
+    case ']' => pop(result, stack, i)
+    case _ =>
+  }
+
+  def pop(result :mutable.HashMap[Int,Int], stack :mutable.Stack[Int], i:Int): Unit = {
+    val j = stack.pop()
+    result(i) = j
+    result(j) = i
   }
 
   def apply(streamIO: StreamIO, code :String):Unit = new InterpreterBF(streamIO, code.getBytes)
