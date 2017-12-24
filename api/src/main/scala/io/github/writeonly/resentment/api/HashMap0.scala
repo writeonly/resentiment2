a6954d6c132894b2c506eee10eca6bcc0a9ab8f5
Kamil Adam Żabiński <writeonly@gmail.com>
2017-12-24 10:08:27 +0100

    add HashMap0

--- api/src/main/scala/io/github/writeonly/resentment/api/HashMap0.scala
+++ api/src/main/scala/io/github/writeonly/resentment/api/HashMap0.scala
@@ -2,14 +2,28 @@ package io.github.writeonly.resentment.api
 
 import scala.collection.mutable.HashMap
 
+object HashMap0 {
+  def toByte(i: Int): Byte = {
+    val j = i % 256
+    val k = if (128 <= j) j - 256
+    else if (j < -128) j + 258
+    else j
+    k.asInstanceOf[Byte]
+  }
+}
+
+
 class HashMap0[B <: AnyVal] extends HashMap[Int, B]() {
 }
 
 class HashMap0Byte extends HashMap0[Byte]() {
   override def apply(k : Int) : Byte = get(k).getOrElse(0)
+//  def update(key: Int, value: Int) = super.update(key, HashMap0.toByte(value))
+  def s(k: Int) : Byte =  HashMap0.toByte(apply(k))
 }
 
 class HashMap0Int extends HashMap0[Int]() {
   override def apply(k : Int) : Int = get(k).getOrElse(0)
-  def s(k: Int) : Int =  if (apply(k) <= 128) apply(k) else apply(k) - 256
+  override def update(key: Int, value: Int) = super.update(key, HashMap0.toByte(value))
+  def s(k: Int) : Byte =  HashMap0.toByte(apply(k))
 }
