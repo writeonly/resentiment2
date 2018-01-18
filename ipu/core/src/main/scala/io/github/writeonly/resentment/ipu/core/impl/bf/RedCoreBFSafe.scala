587d29df268928463e353c966cc0c8376fa2d8a0
Kamil Żabiński <kamil.zabinski@relayr.io>
2018-01-18 22:45:00 +0100

    refactoring

--- ipu/core/src/main/scala/io/github/writeonly/resentment/ipu/core/impl/bf/RedCoreBFSafe.scala
+++ ipu/core/src/main/scala/io/github/writeonly/resentment/ipu/core/impl/bf/RedCoreBFSafe.scala
@@ -7,69 +7,98 @@ class RedCoreBFSafe(core: RedCoreBFUnsafe) extends RedCore[FString] {
 
   def this() = this(new RedCoreBFUnsafe())
 
+  def ne(r: (Int, Int)): Boolean = r._1 != r._2
+
+  def eq(r: (Int, Int)): Boolean = r._1 == r._2
+
   override def rswap(d1: Int, d2: Int): FString =
     if (d1 != d2) core.rswap(d1, d2) else rnop()
 
-  override def rmovc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rmovc(r) else rclr(r._2)
+  override def rmovc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rmovc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
-  override def radd(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.radd(r)
-    else core.mkm(rmovi(2, -3), core.rmul(-3, r._2))
+  override def radd(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.radd(r)
+    case _ if eq(r) => core.mkm(rmovi(2, -3), core.rmul(-3, r._2))
+  }
 
   override def rmovi(r: (Int, Int)): FString = core.rmovi(r)
 
-  override def raddc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.raddc(r) else rclr(r._2)
+  override def raddc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.raddc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
   override def raddi(r: (Int, Int)): FString = core.raddi(r)
 
-  override def rsub(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rsub(r) else rclr(r._2)
+  override def rsub(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rsub(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
-  override def rsubc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rsubc(r) else rclr(r._2)
+  override def rsubc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rsubc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
   override def rsubi(r: (Int, Int)): FString = core.rsubi(r)
 
-  override def rmul(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rmul(r)
-    else core.mkm(rmov(r._1, -3), core.rmul(-3, r._2))
+  override def rmul(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rmul(r)
+    case _ if eq(r) => core.mkm(rmov(r._1, -3), core.rmul(-3, r._2))
+  }
 
-  override def rmov(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rmov(r) else rnop()
+  override def rmov(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rmov(r)
+    case _ if eq(r) => rnop()
+  }
 
   override def rnop(): FString = core.rnop()
 
-  override def rmulc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rmulc(r) else rclr(r._2)
+  override def rmulc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rmulc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
   override def rmuli(r: (Int, Int)): FString = core.rmuli(r)
 
-  override def rdiv(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rdiv(r) else rset(r._2)
+  override def rdiv(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rdiv(r)
+    case _ if eq(r) => rset(r._2)
+  }
 
   override def rset(d: Int): FString = core.rset(d)
 
-  override def rdivc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rdivc(r) else rclr(r._2)
+  override def rdivc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rdivc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
   override def rdivi(r: (Int, Int)): FString = core.rdivi(r)
 
-  override def rmod(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rmod(r) else rclr(r._2)
+  override def rmod(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rmod(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
-  override def rmodc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rmodc(r) else rclr(r._2)
+  override def rmodc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rmodc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
   override def rmodi(r: (Int, Int)): FString = core.rmodi(r)
 
-  override def rpow(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rpow(r)
-    else core.mkm(radd(r._1, -5), core.rpowc(-5, r._2))
+  override def rpow(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rpow(r)
+    case _ if eq(r) => core.mkm(radd(r._1, -5), core.rpowc(-5, r._2))
+  }
 
-  override def rpowc(r: (Int, Int)): FString =
-    if (r._1 != r._2) core.rpowc(r) else rclr(r._2)
+  override def rpowc(r: (Int, Int)): FString = r match {
+    case _ if ne(r) => core.rpowc(r)
+    case _ if eq(r) => rclr(r._2)
+  }
 
   override def rpowi(r: (Int, Int)): FString = core.rpowi(r)
 
